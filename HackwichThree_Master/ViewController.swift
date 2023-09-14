d27c9a3f8f5d97810be78a77ae075a2580c366b8
marionatuhwo <ano@hawaii.edu>
2023-09-13 15:43:42 -1000

    Completed Part 7 - Two problem sets

--- HackwichThree_Master/ViewController.swift
+++ HackwichThree_Master/ViewController.swift
@@ -12,13 +12,20 @@ class ViewController: UIViewController {
 var firstString = "The background color will turn blue"
 var secondString = "The background color will turn green"
 var thirdString = " cool"
+let firstNumber = 1
+let secondNumber = 2
 
+    //IB Outlets for the labels
+    
+    @IBOutlet weak var secondLabel: UILabel!
+    @IBOutlet weak var thirdLabel: UILabel!
     @IBOutlet weak var firstLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-    //Part 
+    //Part 5
     @IBAction func changeColorButtonPressed(_ sender: Any)
     {
         if firstString == "The background color will turn red"
@@ -32,7 +39,38 @@ var thirdString = " cool"
         }
     
     }
+//SOLUTION++++++++++++++++++++++++++++++
+    //PART 7
+    //Problem Set #1
     
-
+    @IBAction func textChangeButtonPressed(_ sender: Any)
+    {
+        if firstString == "The background color will turn blue"
+        {
+            self.secondLabel.text = "My conditional statement evaluates to true"
+        }
+        else
+        {
+            self.thirdLabel.text = "The code in the if block will execute"
+        }
+    }
+    
+    //Problem Set #2
+    
+    @IBAction func additionButtonPressed(_ sender: Any)
+    {
+        
+        if firstNumber > secondNumber
+        {
+            print("The code in this block will not execute")
+        }
+        else
+        {
+        self.view.backgroundColor = UIColor.systemGreen
+        let result = firstNumber + secondNumber
+        print(result)
+        }
+    
+    }
 }
 
